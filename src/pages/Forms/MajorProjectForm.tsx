a4476ac78ca81f4624dac457297f7bc3b19608d2
Sam Cordry <samcordry@gmail.com>
2024-01-31 13:37:56 -0500

    Add form template for submitting major projects

--- src/pages/Forms/MajorProjectForm.tsx
+++ src/pages/Forms/MajorProjectForm.tsx
@@ -6,6 +6,7 @@ import AuthenticationError from '../../callbacks/AuthenticationError'
 import SessionLost from '../../callbacks/SessionLost'
 import UserInfo from '../../UserInfo'
 import UserProfile from '../dashboard/UserProfile'
+import '../../css/major-project-form.css'
 
 const MajorProjectForm = () => {
     // important hooks
@@ -16,7 +17,19 @@ const MajorProjectForm = () => {
 
     return (
         <div>
-            <UserProfile />
+            <h2 className="page-title">Major Project Submission</h2>
+            <form className="major-project-form">
+                <div className="submission-box">
+                    <h6>Project Name</h6>
+                    <input type="text" placeholder="A clever name for your project, sometimes people will come up with an acronym." className="project-name-text-box"></input>
+                </div>
+                <div className="submission-box">
+                    <h6>Description</h6>
+                    <textarea placeholder="A 'two liner' description of what your project is. If you have source materials like a GitHub repo publicly available, it's also useful to include links in them." className="description-text-box"></textarea>
+                </div>
+            </form>
+            <input type="submit" value="SUBMIT MAJOR PROJECT (Route Not Implemented)" className="submit-button"></input>
+            <h2 className="major-project-title">All Major Projects</h2>
         </div>
     )
 }

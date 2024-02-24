e2eba11c21a962b01465dd06faa3d3cf050592ca
Ella Soccoli <soccoli2015@gmail.com>
2024-02-24 16:20:16 -0500

    Made cards that appear on user profile

--- src/pages/dashboard/UserProfile.tsx
+++ src/pages/dashboard/UserProfile.tsx
@@ -9,14 +9,41 @@ const UserProfile: React.FunctionComponent = () => {
     const { accessTokenPayload } = getUseOidcAccessToken()()
     const userInfo = SSOEnabled ? accessTokenPayload as UserInfo : NoSSOUserInfo
 
+    function isActive() {
+        if (userInfo.groups.includes("active")) return "Active";
+        else return "Alumni";
+    }
+
+    function isVoting() {
+        if (userInfo.groups.includes("voting")) return "Voting";
+        else return "Non-Voting";
+    }
+
+    function isOnFloor() {
+        if (userInfo.groups.includes("onfloor")) return "On Floor Status";
+        else return "Off Floor Status";
+    }
+
     return (
-        <Container>
-            <img src={`https://profiles.csh.rit.edu/image/${userInfo.preferred_username}`} alt="User profile picture" />
-            <CardBody>
-                <CardText>{userInfo.name}</CardText>
+        <Container className="flex-container">
+            <img className="pfp" src={`https://profiles.csh.rit.edu/image/${userInfo.preferred_username}`} alt="User profile picture" />
+            <CardBody className="center-vertical">
+                <CardText className="text-item">{userInfo.name}</CardText>
                 {/* CSH email, formatted as username@csh.rit.edu */}
-                <CardText>{userInfo.email}</CardText>
-                {/* need to add the little badges that show active/inactive, voting/non-voting, and on floor/off floor */}
+                <CardText className="text-item">{userInfo.email}</CardText>
+
+                <Container className="inline-cards">
+                    <CardText className={isActive() === "Active" ? "active" : "alumni"}>
+                        {isActive()}
+                    </CardText>
+                    <CardText className={isVoting() === "Voting" ? "voting" : "non-voting"}>
+                        {isVoting()}
+                    </CardText>
+                    <CardText className={isOnFloor() === "On Floor Status" ? "on-floor" : "off-floor"}>
+                        {isOnFloor()}
+                    </CardText>
+                    <CardText className="unused empty"></CardText>
+                </Container>
             </CardBody>
         </Container>
     )

75fad7f35f6f60c6eb0bf238c127552a00548ba6
Ella Soccoli <soccoli2015@gmail.com>
2024-03-12 11:52:39 -0400

    basic structure of more pages

--- src/pages/Attendance/DirectorshipMeeting.tsx
+++ src/pages/Attendance/DirectorshipMeeting.tsx
@@ -1,24 +1,65 @@
-import { useOidcAccessToken, useOidc, useOidcIdToken } from '@axa-fr/react-oidc'
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Authenticating from '../../callbacks/Authenticating'
-import AuthenticationError from '../../callbacks/AuthenticationError'
-import SessionLost from '../../callbacks/SessionLost'
-import UserInfo from '../../UserInfo'
-import UserProfile from '../dashboard/UserProfile'
+import React from 'react';
+import { Container, Form, FormGroup, Label, Input, Button, Col } from 'reactstrap';
 
-const DirectorshipMeeting = () => {
-    // important hooks
-    // const { accessTokenPayload } = useOidcAccessToken()   // this contains the user info in raw json format
-    // const userInfo = accessTokenPayload as UserInfo       //
-    // const { idToken, idTokenPayload } = useOidcIdToken()  // this is how you get the users id token
-    // const { login, logout, isAuthenticated } = useOidc()  // this gets the functions to login and logout and the logout state
-
+const DirectorshipMeeting: React.FC = () => {
     return (
-        <div>
-            <UserProfile />
-        </div>
-    )
-}
+        <Container className="main">
+            <h3 className="page-title">Meeting Attendance</h3>
+            <Form data-module="attendanceForm" data-type="committee">
+                {/* Meeting Type */}
+                <div className="row">
+                    <Col xs={12} sm={12} md={12}>
+                        <div className="panel panel-default">
+                            <div className="panel-body" style={{ paddingTop: '10px' }}>
+                                <FormGroup>
+                                    <Label for="committee" className="control-label">Meeting Type</Label>
+                                    <Input type="select" name="committee" className="form-control">
+                                        <option value="Chairman">Chairman</option>
+                                        <option value="Ad-Hoc">Ad-Hoc</option>
+                                        <option value="Evaluations">Evaluations</option>
+                                        <option value="Financial">Financial</option>
+                                        <option value="R&D">Research and Development</option>
+                                        <option value="House Improvements">House Improvements</option>
+                                        <option value="Opcomm">OpComm</option>
+                                        <option value="History">History</option>
+                                        <option value="Social">Social</option>
+                                        <option value="Public Relations">Public Relations</option>
+                                    </Input>
+                                </FormGroup>
+                            </div>
+                        </div>
+                    </Col>
+                </div>
+                {/* Date */}
+                <div className="row">
+                    <Col xs={12} sm={12} md={12}>
+                        <div className="panel panel-default">
+                            <div className="panel-body" style={{ paddingTop: '10px' }}>
+                                <FormGroup className="is-empty">
+                                    <Label for="date" className="control-label">Date</Label>
+                                    <Input type="text" name="date" className="form-control" value="{date}" data-module="datepicker" />
+                                </FormGroup>
+                            </div>
+                        </div>
+                    </Col>
+                </div>
+                {/* Attendees */}
+                <div className="row">
+                    <Col xs={12} sm={12} md={12}>
+                        <div className="panel panel-default">
+                            <div className="panel-body" style={{ paddingTop: '20px' }}>
+                                <FormGroup className="is-empty">
+                                    <Label for="attendees" className="control-label" id="attendeeLabel">Attendees</Label>
+                                    <Input type="text" name="attendees" className="form-control" data-module="memberSelect" data-src="cm_members" />
+                                </FormGroup>
+                            </div>
+                        </div>
+                    </Col>
+                </div>
+                <Button type="submit" role="button" color="primary" className="btn-raised" style={{ width: '100%' }}>Submit Form</Button>
+            </Form>
+        </Container>
+    );
+};
 
-export default DirectorshipMeeting
+export default DirectorshipMeeting;
--- src/pages/Attendance/TechnicalSeminar.tsx
+++ src/pages/Attendance/TechnicalSeminar.tsx
@@ -1,24 +1,57 @@
-import { useOidcAccessToken, useOidc, useOidcIdToken } from '@axa-fr/react-oidc'
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Authenticating from '../../callbacks/Authenticating'
-import AuthenticationError from '../../callbacks/AuthenticationError'
-import SessionLost from '../../callbacks/SessionLost'
-import UserInfo from '../../UserInfo'
-import UserProfile from '../dashboard/UserProfile'
+import React from 'react';
+import { Container, Row, Col, FormGroup, Label, Input, Button } from 'reactstrap';
 
-const TechnicalSeminar = () => {
-    // important hooks
-    // const { accessTokenPayload } = useOidcAccessToken()   // this contains the user info in raw json format
-    // const userInfo = accessTokenPayload as UserInfo       //
-    // const { idToken, idTokenPayload } = useOidcIdToken()  // this is how you get the users id token
-    // const { login, logout, isAuthenticated } = useOidc()  // this gets the functions to login and logout and the logout state
-
+const SeminarAttendance = () => {
     return (
-        <div>
-            <UserProfile />
-        </div>
-    )
+        <>
+            <Container className="main">
+                <h3 className="page-title">Seminar Attendance</h3>
+                <form data-module="attendanceForm" data-type="seminar">
+                    {/* Seminar Name */}
+                    <Row>
+                        <Col xs="12" sm="12" md="12">
+                            <div className="panel panel-default">
+                                <div className="panel-body" style={{ paddingTop: '10px' }}>
+                                    <FormGroup className="is-empty">
+                                        <Label for="name">Seminar Name</Label>
+                                        <Input type="text" name="name" />
+                                    </FormGroup>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    {/* Date */}
+                    <Row>
+                        <Col xs="12" sm="12" md="12">
+                            <div className="panel panel-default">
+                                <div className="panel-body" style={{ paddingTop: '10px' }}>
+                                    <FormGroup className="is-empty">
+                                        <Label for="date">Date</Label>
+                                        <Input type="text" name="date" value="{{date}}" data-module="datepicker" />
+                                    </FormGroup>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    {/* Attendees */}
+                    <Row>
+                        <Col xs="12" sm="12" md="12">
+                            <div className="panel panel-default">
+                                <div className="panel-body" style={{ paddingTop: '20px' }}>
+                                    <FormGroup className="is-empty">
+                                        <Label for="attendees">Attendees</Label>
+                                        <Input type="text" name="attendees" data-module="memberSelect" data-src="ts_members" />
+                                    </FormGroup>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    {/* Submit Button */}
+                    <Button type="submit" color="primary" style={{ width: '100%' }}>Submit Form</Button>
+                </form>
+            </Container>
+        </>
+    );
 }
 
-export default TechnicalSeminar
+export default SeminarAttendance;
